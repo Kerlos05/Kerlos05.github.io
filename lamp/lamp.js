5bfab2298f001bd0c6d9132847990d9421508a4e
Kerlos <kerlossvensson@gmail.com>
2024-03-13 07:02:01 +0100

    Initial

---
+++ lamp/lamp.js
@@ -0,0 +1,9 @@
+
+document.querySelector('.blub').onclick = function(){
+    document.body.classList.toggle('on'); 
+}
+
+
+document.querySelector('.wrapper').onclick = function(){
+    document.body.classList.toggle('on'); 
+}
